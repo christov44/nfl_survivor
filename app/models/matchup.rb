a2a762b38b7485813fc156418d0001cc739f3511
Christian Baker <cwbaker4@gmail.com>
2016-03-26 17:41:46 -0400

    Manually created records for all team names and weeks

--- app/models/matchup.rb
+++ app/models/matchup.rb
@@ -1,4 +1,6 @@
 class Matchup < ActiveRecord::Base
   has_many :team_matchups
   has_many :teams, through: :team_matchups
+
+  validates :week, uniqueness: {case_sensitive: false}
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,6 @@
 class Team < ActiveRecord::Base
   has_many :team_matchups
   has_many :matchups, through: :team_matchups
+
+  validates :name, uniqueness: {case_sensitive: false}
 end
