f3ae8a39299293b80f19971cce7eb371b8e4605d
Christian Baker <cwbaker4@gmail.com>
2016-04-03 22:20:01 -0400

    Added lives to users, gave each 2 to start with

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160403185427) do
+ActiveRecord::Schema.define(version: 20160404020513) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20160403185427) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "life_count"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
