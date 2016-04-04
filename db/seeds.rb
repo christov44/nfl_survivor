bdfb1693d325425b4cece0e958a77ce608db9914
Christian Baker <cwbaker4@gmail.com>
2016-04-04 18:31:16 -0400

    fixed the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,22 @@
-Week.create(number: 1, number: 2, number: 3, number: 4, number: 5, number: 6, number: 7, number: 8, number: 9, number: 10, number: 11, number: 12, number: 13, number: 14, number: 15, number: 16, number: 17)
+Week.create(number: 1)
+Week.create(number: 2) 
+Week.create(number: 3)
+Week.create(number: 4) 
+# number: 5, number: 6, number: 7, number: 8, number: 9, number: 10, number: 11, number: 12, number: 13, number: 14, number: 15, number: 16, number: 17)
 
-Team.create(name: "Baltimore Ravens", name: "Cincinnati Bengals", name: "Cleveland Browns", name: "Pittsburgh Steelers", name: "Houston Texans",
-name: "Indianapolis Colts", name: "Jacksonville Jaguars", name: "Tennessee Titans", name: "Buffalo Bills", name: "Miami Dolphins", name: "New England Patriots",
-  name: "New York Jets", name: "Denver Broncos", name: "Kansas City Chiefs", name: "Oakland Raiders", name: "San Diego Chargers", name: "Chicago Bears", name: "Detroit Lions", 
-  name: "Green Bay Packers", name: "Minnesota Vikings", name: "Atlanta Falcons", name: "Carolina Panthers", name: "New Orleans Saints", name: "Tampa Bay Buccaneers", 
-  name: "Dallas Cowboys", name: "New York Giants", name: "Philadelphia Eagles", name: "Washington Redskins", name: "Arizona Cardinals", name: "Los Angeles Rams", 
-  name: "San Francisco 49ers", name: "Seattle Seahawks")
+Team.create(name: "Baltimore Ravens")
+Team.create(name: "Cincinnati Bengals")
+Team.create(name: "Cleveland Browns")
+Team.create(name: "Pittsburgh Steelers")
+Team.create(name: "Houston Texans")
+Team.create(name: "Indianapolis Colts")
+Team.create(name: "Jacksonville Jaguars")
+Team.create(name: "Tennessee Titans") 
+Team.create(name: "Buffalo Bills")
+Team.create(name: "Miami Dolphins") 
+Team.create(name: "New England Patriots")
+Team.create(name: "New York Jets")
+ # name: "Denver Broncos", name: "Kansas City Chiefs", name: "Oakland Raiders", name: "San Diego Chargers", name: "Chicago Bears", name: "Detroit Lions", 
+ #  name: "Green Bay Packers", name: "Minnesota Vikings", name: "Atlanta Falcons", name: "Carolina Panthers", name: "New Orleans Saints", name: "Tampa Bay Buccaneers", 
+ #  name: "Dallas Cowboys", name: "New York Giants", name: "Philadelphia Eagles", name: "Washington Redskins", name: "Arizona Cardinals", name: "Los Angeles Rams", 
+ #  name: "San Francisco 49ers", name: "Seattle Seahawks")
