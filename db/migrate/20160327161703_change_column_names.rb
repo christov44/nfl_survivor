797d87066239b146e6014cee83d210235c46c3c9
Christian Baker <cwbaker4@gmail.com>
2016-04-03 23:00:13 -0400

    Deleted some erroneous migrations because Heroku wouldn't rake them

--- db/migrate/20160327161703_change_column_names.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnNames < ActiveRecord::Migration
-  def change
-    rename_column :team_matchups, :team_name_id, :team_id
-    rename_column :team_matchups, :week_id, :matchup_id
-  end
-end
--- db/migrate/20160329003344_delete_table.rb
+++
@@ -1,5 +0,0 @@
-class DeleteTable < ActiveRecord::Migration
-  def change
-    drop_table :team_matchups
-  end
-end
