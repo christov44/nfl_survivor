d97ab7abd17a93090eda75bf0e6f72a9f7bfb906
Christian Baker <cwbaker4@gmail.com>
2016-04-04 18:23:48 -0400

    created a seed file, added the get_user_lives method to user model, and modified the create_blank_picks method in the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,18 +6,19 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   after_create :create_blank_picks
-  # after_create :give_user_lives
+  after_create :give_user_lives
 
   private
     def create_blank_picks #At some point I could just go clear the Week's db and make the week_ids and week_numbers line up
-      (1..5).each do |week|
+      (1..17).each do |week|
         Pick.create(user_id: self.id, week_id: week)
       end
-      (7..18).each do |week|
-        Pick.create(user_id: self.id, week_id: week)
-      end
+      # (7..18).each do |week|
+      #   Pick.create(user_id: self.id, week_id: week)
+      # end
     end
 
-    # def give_user_lives
-    # end
+    def give_user_lives
+      User.last.life_count = 2
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Week.create(number: 1, number: 2, number: 3, number: 4, number: 5, number: 6, number: 7, number: 8, number: 9, number: 10, number: 11, number: 12, number: 13, number: 14, number: 15, number: 16, number: 17)
+
+Team.create(name: "Baltimore Ravens", name: "Cincinnati Bengals", name: "Cleveland Browns", name: "Pittsburgh Steelers", name: "Houston Texans",
+name: "Indianapolis Colts", name: "Jacksonville Jaguars", name: "Tennessee Titans", name: "Buffalo Bills", name: "Miami Dolphins", name: "New England Patriots",
+  name: "New York Jets", name: "Denver Broncos", name: "Kansas City Chiefs", name: "Oakland Raiders", name: "San Diego Chargers", name: "Chicago Bears", name: "Detroit Lions", 
+  name: "Green Bay Packers", name: "Minnesota Vikings", name: "Atlanta Falcons", name: "Carolina Panthers", name: "New Orleans Saints", name: "Tampa Bay Buccaneers", 
+  name: "Dallas Cowboys", name: "New York Giants", name: "Philadelphia Eagles", name: "Washington Redskins", name: "Arizona Cardinals", name: "Los Angeles Rams", 
+  name: "San Francisco 49ers", name: "Seattle Seahawks")
